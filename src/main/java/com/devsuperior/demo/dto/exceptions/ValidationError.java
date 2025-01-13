219b2777e1ac2284277cd4b33efbb8a0aa3cbf98
David Alves Dutra <dadutra@hotmail.com>
2025-01-13 15:55:00 -0300

    Adicionados dtos de retorno dos erros/exceções.

---
+++ src/main/java/com/devsuperior/demo/dto/exceptions/ValidationError.java
@@ -0,0 +1,23 @@
+package com.devsuperior.demo.dto.exceptions;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError{
+    private List<FieldMessage> errors = new ArrayList<FieldMessage>();
+
+    public ValidationError() {}
+    public ValidationError(Instant timestamp, int status, String error, String path, String message) {
+        super(timestamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
