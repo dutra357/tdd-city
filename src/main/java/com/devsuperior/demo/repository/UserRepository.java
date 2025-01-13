62701fd6e2676e6ae89ec5d2a17d7082ad764f86
David Alves Dutra <dadutra@hotmail.com>
2025-01-13 15:53:58 -0300

    Adicionados Repositories e classe User.

---
+++ src/main/java/com/devsuperior/demo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.demo.repository;
+
+import com.devsuperior.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
