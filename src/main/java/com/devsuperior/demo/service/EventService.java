1ac372d7e4a5eddad06fa8c62860e79d9704a07e
David Alves Dutra <dadutra@hotmail.com>
2025-01-13 16:06:14 -0300

    Adicionados Services, com UserDatailsService implementado para User.

---
+++ src/main/java/com/devsuperior/demo/service/EventService.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demo.service;
+
+import com.devsuperior.demo.repository.EventRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+    private final EventRepository repository;
+    public EventService(EventRepository repository) {
+        this.repository = repository;
+    }
+}
--- src/main/java/com/devsuperior/demo/service/UserService.java
+++ src/main/java/com/devsuperior/demo/service/UserService.java
@@ -1,4 +1,25 @@
 package com.devsuperior.demo.service;
 
-public class UserService {
+import com.devsuperior.demo.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository repository;
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder, UserRepository repository) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.repository = repository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
